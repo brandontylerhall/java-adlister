caf4e414bd4f5cd2ad342b5d1de8f653c9af5b8a
Brandon Hall <bran.tyler.hall@gmail.com>
2023-02-01 18:56:32 -0500

    completed exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,47 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    private int pageCount;
 
+    public void init() {
+        // Reset hit counter.
+        pageCount = 0;
+    }
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        int count;
+        String name;
+        try {
+            count = Integer.parseInt(req.getParameter("count"));
+        } catch (Exception e) {
+            count = 1;
+        }
+        name = req.getParameter("name");
+        if (name == null) {
+            name = "World";
+        }
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        for (int i = 0; i < count; i++) {
+            out.println("<h1>Hello, " + name + "</h1>");
+        }
+
+
+        pageCount++;
+        String title = "Total Number of Hits";
+        String docType = "<!doctype html public \"-//w3c//dtd html 4.0 " + "transitional//en\">\n";
+
+        out.println(docType +
+                "<html>\n" +
+                "<head><title>" + title + "</title></head>\n" +
+                "<h1 align = \"center\">" + title + "</h1>\n" +
+                "<h2 align = \"center\">" + pageCount + "</h2>\n" +
+                "</html>"
+        );
     }
 }
